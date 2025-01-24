7db2b3caef22886c5cee778586a2c7a0a341bb2e
Anton Nyström <35500074+antonnyst@users.noreply.github.com>
2025-01-24 10:09:13 +0100

    Add basic Dockerfile and mariadb

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine:3.20
+
+RUN apk add --no-cache mariadb 
+
+RUN adduser admin -D -H
+
+ADD start.sh /
+
+RUN chmod +x /start.sh 
+
+EXPOSE 3306
+
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,47 @@
+#!/bin/ash
+# From https://community.hetzner.com/tutorials/dockerizing-mariadb-with-alpine-linux
+
+set -e
+
+USER=root
+DIR_DATA=/var/lib/maria
+
+signal_terminate_trap() {
+    #
+    # Shutdown MariaDB with mariadb-admin
+    # https://mariadb.com/kb/en/mariadb-admin/
+    mariadb-admin --user=$USER shutdown &
+    #
+    # Wait for mariadb-admin until sucessfully done (exit)
+    wait $!
+    echo "MariaDB shut down successfully"
+}
+
+trap "signal_terminate_trap" SIGTERM
+
+if [ ! -f "$DIR_DATA/ibdata1" ]; then
+    initialize_status="MariaDB initialization done"
+
+    # Initialize MariaDB with mariadb-install-db
+    # https://mariadb.com/kb/en/mariadb-install-db/
+    mariadb-install-db \
+        --user=$USER \
+        --datadir=$DIR_DATA \
+        --auth-root-authentication-method=socket &
+    #
+    # Wait for mariadb-install-db until sucessfully done (exit)
+    wait $!
+fi
+
+echo $initialize_status
+
+# Run
+echo "Starting MariaDB ..."
+#
+# Run MariaDB with exec bash command
+exec mariadbd --user=$USER --datadir=$DIR_DATA --socket=/var/lib/maria/maria.sock &
+#
+# Wait for MariaDB until stopped by Docker
+wait $!
+exit 1
+
